d21ffa579bde0bee86e7805eff55abe9dc380940
Rathindra Nath Biswas <rathininfo7@gmail.com>
2026-09-02 11:34:41 +0600

    Question 2 solve

--- answer.js
+++ answer.js
@@ -1,19 +1,40 @@
 // Question No 1
-function describeValue(value){
-    let type = typeof value;
+// function describeValue(value){
+//     let type = typeof value;
 
-    let truthness
+//     let truthness
 
-    if(value){
-       truthness = "truthy"
-    }
-    else {
-        truthness = "falsy"
-    }
+//     if(value){
+//        truthness = "truthy"
+//     }
+//     else {
+//         truthness = "falsy"
+//     }
 
-    return `${type} | ${truthness} `
+//     return `${type} | ${truthness} `
+// }
+
+// console.log(describeValue(""));
+
+
+// Question No 2
+function getDayType(day){
+ switch (day) {
+    case "Friday": 
+     case "Saturday":
+     return "Weekend Day"
+        case "Sunday":
+            case "Monday":
+                case "Tuesday":
+                    case "Wednesday":
+                        case "Thursday" :    
+                        return  "Working Day"
+    default:
+        return "Invalid Day"
+ }
+
+ 
 }
 
-console.log(describeValue(""));
-
+console.log(getDayType("Monday"))
 
