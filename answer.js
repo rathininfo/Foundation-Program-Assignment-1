5d2e623d006befdf8305091950563ff3d9cd4566
Rathindra Nath Biswas <rathininfo7@gmail.com>
2026-09-02 15:46:29 +0600

    completed full assignment

--- answer.js
+++ answer.js
@@ -1,67 +1,66 @@
-// Question No 1
-// function describeValue(value){
-//     let type = typeof value;
 
-//     let truthness
+function describeValue(value){
+    let type = typeof value;
 
-//     if(value){
-//        truthness = "truthy"
-//     }
-//     else {
-//         truthness = "falsy"
-//     }
+    let truthness
 
-//     return `${type} | ${truthness} `
-// }
+    if(value){
+       truthness = "truthy"
+    }
+    else {
+        truthness = "falsy"
+    }
 
-// console.log(describeValue(""));
+    return `${type} | ${truthness} `
+}
 
 
-// Question No 2
-// function getDayType(day){
-//  switch (day) {
-//     case "Friday": 
-//      case "Saturday":
-//      return "Weekend Day"
-//         case "Sunday":
-//             case "Monday":
-//                 case "Tuesday":
-//                     case "Wednesday":
-//                         case "Thursday" :    
-//                         return  "Working Day"
-//     default:
-//         return "Invalid Day"
-//  }
+
+
+
+function getDayType(day){
+ switch (day) {
+    case "Friday": 
+     case "Saturday":
+     return "Weekend Day"
+        case "Sunday":
+            case "Monday":
+                case "Tuesday":
+                    case "Wednesday":
+                        case "Thursday" :    
+                        return  "Working Day"
+    default:
+        return "Invalid Day"
+ }
 
  
-// }
-
-// console.log(getDayType("Monday"))
+}
 
 
-// Question No 3
 
-// function validateUsername(username){
-//  let usersLength = username.length
-//   if(usersLength < 4){
-//     return "Too Short"
-//   }
-//   if(username.includes(" ")){
-//     return "No Space Allowed"
-//   }
+
+
+function validateUsername(username){
+ let usersLength = username.length
+  if(usersLength < 4){
+    return "Too Short"
+  }
+  if(username.includes(" ")){
+    return "No Space Allowed"
+  }
    
   
-//   if(username.toLowerCase().includes("admin") ){
-//     return "Reserved Word"
-//   }
+  if(username.toLowerCase().includes("admin") ){
+    return "Reserved Word"
+  }
 
-//    return "Available"
+   return "Available"
 
-// }
+}
 
-// console.log(validateUsername("Admin_Rahim"))
+console.log(validateUsername("Admin_Rahim"))
 
-// Question No 4
+
 
 function getCngFare(distance, isNight, waitingMinutes){
 
@@ -93,4 +92,37 @@ function getCngFare(distance, isNight, waitingMinutes){
     return fare;
 
 }
+
+
+
+
+const getChaseVerdict = (target, scored, ballsLeft) => {
+
+ const runsNeeded =  target - scored;
+
+ if(runsNeeded <= 0 ){
+    return "Won"
+ }
+
+ if(ballsLeft <= 0 ){
+    return "Lost"
+ }
+
+ const requiredRate = (runsNeeded / ballsLeft) * 6;
+  let verdict ;
+
+  if(requiredRate <= 6){
+    verdict = "Comfortable"
+  }
+  else if(requiredRate <= 12){
+    verdict = "Tough";
+  }
+
+  else{
+    verdict = "Almost Impossible";
+  }
+
+  return "need" + runsNeeded + "runs in" + ballsLeft + " balls | " + verdict;
+ 
+};
 
